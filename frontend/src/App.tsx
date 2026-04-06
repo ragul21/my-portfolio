9d9d8af276765e7684ae79d01dd4b40ea9cf9385
Ragul <ragulragul8788@gmail.com>
2026-04-06 12:32:39 +0530

    initial commit : React + Vite setup + tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-900">
+      <h1 className="text-4xl font-bold text-white transition-colors hover:text-blue-500">
+        Hello, React + Tailwind v4!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
